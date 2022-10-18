69a6e7cfd62d523df1e897d619a40529933d3407
Raymond Langas <r_langas@fanshaweonline.ca>
2022-10-18 01:10:35 -0400

    Added cart, still a WIP

--- frontend/src/pages/Order.tsx
+++ frontend/src/pages/Order.tsx
@@ -1,27 +1,52 @@
-import React, { useState } from "react";
+import { stringify } from "querystring";
+import React, { useState, useReducer } from "react";
 
 type Props = {};
 
+const categories = [ "A", "B", "C" ]
+
+const items = [
+    { id: 0, name: "Item A1", category: "A", price: 99.99, stock: 10 },
+    { id: 1, name: "Item A2", category: "A", price: 99.99, stock: 10 },
+    { id: 2, name: "Item A3", category: "A", price: 99.99, stock: 10 },
+    { id: 3, name: "Item B1", category: "B", price: 99.99, stock: 10 },
+    { id: 4, name: "Item B2", category: "B", price: 99.99, stock: 10 },
+    { id: 5, name: "Item B3", category: "B", price: 99.99, stock: 10 },
+    { id: 6, name: "Item C1", category: "C", price: 99.99, stock: 10 },
+    { id: 7, name: "Item C2", category: "C", price: 99.99, stock: 10 },
+    { id: 8, name: "Item C3", category: "C", price: 99.99, stock: 10 },
+]
+
 const Order = (props: Props) => {
-    const [category, setCategory] = useState("A");
+    const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+    const [cart, setCart] = useState<{name: string, category: string, price: number, qty: number}[]>([],);
 
-    const categories = [ "A", "B", "C" ]
+    const categoryClick = (category: string) => {
+        setSelectedCategory(category);
+    }
 
-    const items = [
-        { name: "Item A1", category: "A", price: 99.99, qty: 10 },
-        { name: "Item A2", category: "A", price: 99.99, qty: 10 },
-        { name: "Item A3", category: "A", price: 99.99, qty: 10 },
-        { name: "Item B1", category: "B", price: 99.99, qty: 10 },
-        { name: "Item B2", category: "B", price: 99.99, qty: 10 },
-        { name: "Item B3", category: "B", price: 99.99, qty: 10 },
-        { name: "Item C1", category: "C", price: 99.99, qty: 10 },
-        { name: "Item C2", category: "C", price: 99.99, qty: 10 },
-        { name: "Item C3", category: "C", price: 99.99, qty: 10 },
+    const productClick = (itemToAdd: any) => {
+        if(cart.length !== 0)
+        {
+            // loop through cart, if found increment qty and update price
+            var cartItem;
+            if(cartItem = cart.find((cart) => cart.name === itemToAdd.name))
+            {
+                cartItem.qty++;
+                cartItem.price += itemToAdd.price;
+            }
+            // if item was not in cart, add to cart with inital qty of 1
+            else
+                setCart([...cart, { name: itemToAdd.name, category: itemToAdd.category, price: itemToAdd.price, qty: 1}]);
+        }
+        else
+        {
+            setCart([{ name: itemToAdd.name, category: itemToAdd.category, price: itemToAdd.price, qty: 1}]);
+        }
         
-    ]
 
-    const catClick = (cat: string) => {
-        setCategory(cat);
+        // decrement stock
+        items[itemToAdd.id].stock--;
     }
 
     return (
@@ -29,10 +54,10 @@ const Order = (props: Props) => {
             <div className="text-center p-5">Order</div>
             {/* Categories */}
             <div className="flex flex-wrap justify-center">
-                { categories.map((cat) => (
+                { categories.map((category) => (
                     <div>
-                        <button className="bg-orange-300 m-1 rounded-lg hover:bg-orange-500" onClick={() => catClick(cat)}>
-                            <div className="w-40">{cat}</div>
+                        <button className="bg-orange-300 m-1 rounded-lg hover:bg-orange-500" onClick={() => categoryClick(category)}>
+                            <div className="w-40">{category}</div>
                         </button>
                     </div>
                 ))}
@@ -41,16 +66,30 @@ const Order = (props: Props) => {
             {/* Items */}
             <div className="flex flex-wrap justify-center">
                 {items.map(item => {
-                    return item.category === category
+                    return item.category === selectedCategory
                             ?   <div>
-                                    <button className="flex flex-row w-max bg-blue-300 m-1 rounded-lg hover:bg-blue-500">
+                                    <button className="flex flex-row w-max bg-blue-300 m-1 rounded-lg hover:bg-blue-500" onClick={() => productClick(item)}>
                                         <div className="basis-1/2 m-3 whitespace-nowrap">{item.name}</div>
-                                        <div className="basis-1/4 m-3">{item.price}</div>
-                                        <div className="basis-1/4 m-3">{item.qty}</div>
+                                        <div className="basis-1/4 m-3">${item.price}</div>
+                                        <div className="basis-1/4 m-3">{item.stock}</div>
                                     </button>
                                 </div>
                             :   <div/>
                 })}
+            </div>
+
+            {/* Cart
+                WIP: Will only show updated cart if a new item is added to cart, added same product will not refresh display
+            */}
+            <div className="flex flex-col mt-52 p-2 bg-slate-100">
+                Cart:
+                {cart.map(product => (
+                    <div className="flex flex-row">
+                        <div className="basis-1/2 m-3 whitespace-nowrap">{product.name}</div>
+                        <div className="basis-1/4 m-3">${product.price}</div>
+                        <div className="basis-1/4 m-3 text-right">{product.qty}</div>
+                    </div>
+                ))}
             </div>
         </div>
     );
