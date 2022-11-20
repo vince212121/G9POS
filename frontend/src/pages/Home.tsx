683e3014a580d9fe68689f9f1d7088769d2d9a84
vince212121 <54919042+vince212121@users.noreply.github.com>
2022-11-19 19:01:10 -0500

    fixed more styling

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import { SliderData } from "../assets/data";
 import ImageSlider from "../components/ImageSlider";
-import Error from "../components/Error";
 
 import purchase from "../images/purchase.png";
 import orderHistoryVendor from "../images/orderhistory.png";
@@ -56,53 +55,37 @@ const Home = (props: Props) => {
 
   return (
     <div className="w-screen">
-      <div className="flex flex-wrap justify-center space-x-5 border-2 rounded-lg m-5 p-5 h-100 w-200">
-        {/* Page buttons */}
-        {/* <div className="border-2 rounded-lg m-5 p-5">
-            <div className="flex flex-wrap justify-center space-x-5">
-              <Link to="/order" className="bg-blue-500 rounded-md p-5">
-                Orders
-              </Link>
-              <Link to="/pastorders" className="bg-blue-500 rounded-md p-5">
-                Order History
-              </Link>
-              <Link to="/customers" className="bg-blue-500 rounded-md p-5">
-                Customer Info
-              </Link>
-            </div>
-          </div> */}
+      <div className="flex flex-wrap justify-center space-x-5 border-2 rounded-lg m-5 p-5">
         <div>
           <main>
-            <div>
-              <div className="columns-3">
+            <div className="">
+              <div className="flex flex-col items-center justify-center md:columns-3 md:block">
                 {menus.map((menu) => (
-                  <div>
-                    <button
-                      className="w-80 h-50 rounded-lg hover:bg-orange-500"
-                      onClick={() => navigate(menu.link)}
-                    >
-                      <div className="w-60 h-60 ml-10">
-                        {menu.menu}
-                        <img
-                          className="w-40 h-40  ml-10"
-                          alt={menu.menu}
-                          src={menu.image}
-                        ></img>
-                      </div>
-                    </button>
-                  </div>
+                  <button
+                    className="lg:w-80 lg:h-50 rounded-lg hover:bg-orange-500 flex flex-col justify-center items-center p-4"
+                    onClick={() => navigate(menu.link)}
+                    key={menu.menu}
+                  >
+                    {/* <div className="w-60 h-60 ml-10">
+
+                    </div> */}
+                    {menu.menu}
+                    <img
+                      className="w-20 h-20 md:w-40 md:h-40"
+                      alt={menu.menu}
+                      src={menu.image}
+                    />
+                  </button>
                 ))}
               </div>
             </div>
           </main>
-        </div>
-        {/* Info panel (you can put store information here) */}
-        <div className="border-2 rounded-lg m-5 p-5">
-          <div className="flex flex-wrap justify-center space-x-5">
-            <div className="p-5 bg-red-100">Store Name</div>
-            <div className="p-5 bg-red-100">100%</div>
-            <div className="p-5 bg-red-100">250 items</div>
-            <div className="p-5 bg-red-100">$500</div>
+          {/* Info panel (you can put store information here) */}
+          <div className="flex flex-col md:flex-row justify-center items-center space-y-5 md:space-y-0 md:space-x-5 border-2 rounded-lg m-5 p-5">
+            <span className="p-5 bg-red-100">Store Name</span>
+            <span className="p-5 bg-red-100">100%</span>
+            <span className="p-5 bg-red-100">250 items</span>
+            <span className="p-5 bg-red-100">$500</span>
           </div>
         </div>
       </div>
