d4ea53769769fc636d976435662b84ddcff48ded
unknown <zmsdong@naver.com>
2022-12-02 18:09:27 -0500

    Added FAQ page

---
+++ frontend/src/components/accordion.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+type Props = {};
+
+const Accordian = (props: Props) => {
+  const accordionData = {
+    title: "Question",
+    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+          laborum cupiditate possimus labore, hic temporibus velit dicta earum
+          suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+          voluptatem.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+          laborum cupiditate possimus labore, hic temporibus velit dicta earum
+          suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+          voluptatem.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+          laborum cupiditate possimus labore, hic temporibus velit dicta earum
+          suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+          voluptatem.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+          laborum cupiditate possimus labore, hic temporibus velit dicta earum
+          suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+          voluptatem.`,
+  };
+
+
+
+
+  const { title, content } = accordionData;
+
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <React.Fragment>
+      <div className="border-solid border-4 w-3/4">
+        <div>
+          <div onClick={() => setIsActive(!isActive)}>
+            <div className="flex flex-row bg-slate-200	">
+              {title}
+              <div>
+                {isActive ? (
+                  <svg
+                    className="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"
+                    ></path>
+                  </svg>
+                ) : (
+                  <svg
+                    className="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"
+                    ></path>
+                  </svg>
+                )}
+              </div>
+            </div>
+          </div>
+          {isActive && <div className="bg-slate-200	">{content}</div>}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+export default Accordian;
--- frontend/src/pages/footer/FAQ.tsx
+++ frontend/src/pages/footer/FAQ.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Accordion from "../../components/accordion";
+
 
 type Props = {};
 
@@ -7,6 +9,10 @@ const FAQ = (props: Props) => {
   return (
     <div className="lg:mx-4 my-8 w-screen flex flex-col items-center space-y-4">
       <h2 className="text-xl font-bold">FAQ</h2>
+      <h1> Freqently Asked Question</h1>
+      <Accordion/>
+      <Accordion/>
+      <Accordion/>
     </div>
   );
 };
