6a768cc913347ebc2a06d96431482a76962baf96
Vincent <vincentli.dev@gmail.com>
2022-12-20 13:05:07 -0500

    removed key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#ispy(c&#y_1*s3nf$t)vwdpvfv3r!ui0=h2k5j2ayh$nu+@k5'
+SECRET_KEY = 'Some key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
